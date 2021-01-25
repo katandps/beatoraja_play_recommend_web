37351260fa9290032846e34aa2ccdeaeaf316e54
katand <katandps@gmail.com>
2021-01-25 14:52:28 +0900

    名前の変更の際、クッキーを見ていたのでログイン状況と変更の可否がずれてしまっていたのを修正 レイアウトも調整

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -1,36 +1,51 @@
 <template>
   <div id="profile-edit">
     <h1>プロフィール</h1>
-    <label>
-      名前:
-      <input class="form-control" v-model="name">
-    </label>
-    <button class="btn btn-success" @click="change_name">変更を反映</button>
-    <br />
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <label for="name-input" class="btn btn-outline-secondary">
+          名前</label>
+      </div>
+      <input id="name-input" class="form-control" v-model="name">
+      <div class="input-group-append">
+        <label class="btn btn-success text-nowrap" @click="change_name" :disabled="lock">
+          変更を反映
+        </label>
+      </div>
+    </div>
+    <br/>
     {{ message }}
   </div>
 </template>
 
 <script>
 import Api from "../api.js"
+import * as log from "loglevel"
 
 export default {
   name: "ProfileEdit",
   data: () => ({
     name: "",
     message: "",
+    lock: false,
   }),
   mounted() {
-    this.name = this.$store.getters.userInfo.user_name;
+    log.debug(this.$store.getters.userInfo)
+    this.name = this.$store.getters.userInfo.name
   },
   methods: {
     async change_name() {
-      if (this.name === "" || this.name === this.$store.getters.userInfo.user_name) {
-        return;
+      if (this.name === "" || this.name === this.$store.getters.userInfo.name) {
+        return
       }
-      let res = await Api.change_user_name(this.$cookies.get('session-token'), this.name);
-      this.$store.commit("setUserInfo", res);
-      this.message = "" + this.name + "に更新しました。"
+      this.lock = true
+      await Api.change_user_name(this.$store.getters.accessToken, this.name).then(res => {
+        this.$store.commit("setUserInfo", res)
+        this.message = "" + this.name + "に更新しました。"
+        this.lock = false
+      }).catch(() => {
+        this.message = "更新に失敗しました。"
+      });
     }
   }
 }
