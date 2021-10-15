25985fad9568e3c25fb55632ba749f1d395f63cc
katand <katandps@gmail.com>
2021-10-15 15:16:26 +0900

    github actionsでビルドできるよう調整

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,7 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
     publicPath: './',
     outputDir: 'docs',
-    configureWebpack: {
-        plugins: [new BundleAnalyzerPlugin()]
-    },
     devServer: {
         disableHostCheck: true
-    }
+    },
 }
