d7c84153178f435936c9b9a576cc7dafbfa06d43
katand <katandps@gmail.com>
2022-05-02 06:08:40 +0900

    特定モーダル専用だったコンポーネントをモーダルに統合

--- src/components/top_route/score_viewer/modal/RecommendModal.vue
+++ src/components/top_route/score_viewer/modal/RecommendModal.vue
@@ -4,24 +4,50 @@
        <h2>おすすめ設定</h2>
     </template>
     <template v-slot:body>
-      <filter-preset/>
+      <div class="filter">
+        <h3>更新狙い</h3>
+        <button class="btn btn-success" @click="filter.for_score()">スコア更新狙い</button>
+        <button class="btn btn-success" @click="filter.for_bp()">BP更新狙い</button>
+
+        <h3>スコアランク狙い</h3>
+        <button class="btn btn-success" @click="filter.for_aaa()">AAA狙い</button>
+        <button class="btn btn-success" @click="filter.for_aa()">AA狙い</button>
+
+        <h3>クリアランプ狙い</h3>
+        <button class="btn btn-success" @click="filter.for_easy()">Easy狙い</button>
+        <button class="btn btn-success" @click="filter.for_hard()">Hard狙い</button>
+        <button class="btn btn-success" @click="filter.for_ex_hard()">EXH狙い</button>
+        <button class="btn btn-success" @click="filter.for_full_combo()">FC狙い</button>
+
+        <h3>ランダムセレクト</h3>
+        <button class="btn btn-primary" @click="filter.for_random()">ランダムセレクト</button>
+      </div>
     </template>
   </modal-base>
 </template>
 
 <script>
-import FilterPreset from "../selector/FilterPreset"
 import ModalBase from "./ModalBase"
 
 export default {
   name: "RecommendModal",
-  components: {ModalBase, FilterPreset},
+  components: {ModalBase},
   methods: {
     show_modal() {
       this.$refs.modal_base.show_modal()
     },
   },
+  computed: {
+    filter() {
+      return this.$store.getters.filter
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.filter button {
+  margin: 5px;
+  display: inline
+}
+</style>
