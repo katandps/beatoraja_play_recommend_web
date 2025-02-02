9f9f58f7c0b827ed2397f4b3c06028b68c7c6011
katandps <katandps@gmail.com>
2025-02-02 10:57:17 +0900

    add calendar page

---
+++ src/components/top_route/my_page/PlayCalendar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useLoginStore } from '@/store/session'
+
+const store = useLoginStore()
+
+const message = ref("")
+</script>
+
+<template>
+    <div id="play_calendar" v-if="store.userInfo">
+        <h2>プレイカレンダー</h2>
+        {{ message }}
+    </div>
+</template>
+
+<style scoped>
+#play_calendar {
+    padding-top: 20px
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -5,13 +5,14 @@ import RecommendTableVue from "./components/top_route/RecommendTable.vue"
 import ScoreViewerVue from "./components/top_route/ScoreViewer.vue"
 import SongRankingVue from "./components/top_route/SongRanking.vue"
 import TheHomeVue from "./components/top_route/TheHome.vue"
+import PlayCalendar from "./components/top_route/my_page/PlayCalendar.vue"
 
 const routes = [
   { path: "/", component: async () => TheHomeVue },
   {
     path: "/view",
     component: ScoreViewerVue,
-    props: (route:any) => ({
+    props: (route: any) => ({
       user_id: parseInt(route.query.user_id || 0),
       rival_id: parseInt(route.query.rival_id || 0),
       mode: "detail"
@@ -20,27 +21,31 @@ const routes = [
   {
     path: "/view/:mode",
     component: ScoreViewerVue,
-    props: (route:any) => ({
+    props: (route: any) => ({
       user_id: parseInt(route.query.user_id || 0),
       rival_id: parseInt(route.query.rival_id || 0),
       mode: route.params.mode
     }),
   },
-  { path: "/mypage", component: MyPageVue},
+  { path: "/mypage", component: MyPageVue },
   {
     path: "/stats",
     component: PlayStatsVue,
-    props: (route:any) => ({ user_id: parseInt(route.query.user_id) })
+    props: (route: any) => ({ user_id: parseInt(route.query.user_id) })
   },
   {
     path: "/table",
-    component:  RecommendTableVue,
-    props: (route:any) => ({ user_id: parseInt(route.query.user_id) })
+    component: RecommendTableVue,
+    props: (route: any) => ({ user_id: parseInt(route.query.user_id) })
   },
   {
     path: "/song",
     component: SongRankingVue,
-    props: (route:any) => ({ sha256: route.query.sha256, date: route.query.date })
+    props: (route: any) => ({ sha256: route.query.sha256, date: route.query.date })
+  },
+  {
+    path: "/calendar",
+    component: PlayCalendar
   },
 ]
 export const router = createRouter({
